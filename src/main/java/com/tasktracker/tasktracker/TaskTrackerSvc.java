fb2adf8898a852d1dfdf863136894accc9c33a65
breannar888 <67029835+breannar888@users.noreply.github.com>
2024-02-07 15:22:27 -0600

    rename main class

--- src/main/java/com/tasktracker/tasktracker/TaskTrackerSvc.java
+++ src/main/java/com/tasktracker/tasktracker/TaskTrackerSvc.java
@@ -3,8 +3,8 @@ package com.tasktracker.tasktracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class TaskTrackerApplication {
+public class TaskTrackerSvc {
 	public static void main(String[] args) {
-		SpringApplication.run(TaskTrackerApplication.class, args);
+		SpringApplication.run(TaskTrackerSvc.class, args);
 	}
 }
